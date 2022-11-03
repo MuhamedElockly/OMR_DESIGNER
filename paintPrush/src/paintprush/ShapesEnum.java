4e7520a80c129551e3e474aab49bcefacbd9806b
MuhamedElockly <elocklymuhamed@gmail.com>
2022-11-03 15:38:12 +0200

    LastEdit

--- paintPrush/src/paintprush/ShapesEnum.java
+++
@@ -1,6 +0,0 @@
-
-package paintprush;
-
-public enum ShapesEnum {
-    Line,Circle,Square,Rectangle,Oval,RoundRect,Triangle,Hexagon,Bentagon,Heptagon,Pencil,SmilyFace1,SmilyFace2,SmilyFace3,CompositShape,Text
-}
